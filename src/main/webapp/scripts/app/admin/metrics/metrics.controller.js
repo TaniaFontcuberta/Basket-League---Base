164438c3cb27ac7fd1437cc694f2ba35058c0d17
Tania Fontcuberta Mercadal <taniafontcubertamercadal@gmail.com>
2015-12-01 15:13:46 +0100

    Update JHipster 2.24

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('basketLeagueApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
